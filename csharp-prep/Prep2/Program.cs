73bfcc395247775179ef18f1112dca39585ffd51
solomonitie <solomonitie@gmail.com>
2024-09-23 11:10:21 -0700

    Prep1 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage!");
+        int grade = Console.ReadLine();
+
+        if(grade >= 90)
+        {
+            Console.WriteLine("Your Grade is --> A");
+        }
+
+        if(grade >= 90)
+        {
+            Console.WriteLine("Your Grade is --> A");
+        }
+
+        else if(grade >= 80)
+        {
+            Console.WriteLine("Your Grade is --> B");
+        }
+
+        else if(grade >= 70)
+        {
+            Console.WriteLine("Your Grade is --> C");
+
+        }
+
+        else if(grade >= 60)
+        {
+            Console.WriteLine("Your Grade is --> E");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Your Grade is --> F");
+        }
+
+        
     }
 }
